88c71b61d8b45d0ab0b72260c0ceb52967e42c5c
FirdausyTama <atama.firdausy@gmail.com>
2024-12-28 22:31:59 +0700

    membuat entrybody button jika form sudah di input

--- app/src/main/java/com/example/restapi/ui/view/InsertView.kt
+++ app/src/main/java/com/example/restapi/ui/view/InsertView.kt
@@ -1,19 +1,31 @@
 package com.example.restapi.ui.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.restapi.navigation.DestinasiNavigasi
 import com.example.restapi.ui.costumewidget.CostumeTopAppBar
+import com.example.restapi.ui.viewmodel.InsertUiEvent
+import com.example.restapi.ui.viewmodel.InsertUiState
 import com.example.restapi.ui.viewmodel.InsertViewModel
 import com.example.restapi.ui.viewmodel.PenyediaViewModel
+import kotlinx.coroutines.launch
 
 object DestinasiEntry:DestinasiNavigasi{
     override val route = "item_entry"
@@ -41,6 +53,56 @@ fun EntryMhsScreen(
             )
         }
     ){innerPadding ->
-
+        EntryBody(
+            insertUiState = viewModel.uiState,
+            onSiswaValueChange = viewModel::updateInsertMhsState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.insertMhs()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
     }
+}
+
+@Composable
+fun EntryBody(
+    insertUiState:InsertUiState,
+    onSiswaValueChange:(InsertUiEvent)-> Unit,
+    onSaveClick:() -> Unit,
+    modifier: Modifier=Modifier
+){
+    Column (
+        verticalArrangement = Arrangement.spacedBy(18.dp),
+        modifier = modifier.padding(12.dp)
+    ){
+        FormInput(
+            insertUiEvent = insertUiState.insertUiEvent,
+            onValueChange = onSiswaValueChange,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onSaveClick,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Simpan")
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormInput(
+    insertUiEvent: InsertUiEvent,
+    modifier: Modifier=Modifier,
+    onValueChange:(InsertUiEvent) -> Unit = {},
+    enabled: Boolean = true
+){
+    Column (){  }
 }
