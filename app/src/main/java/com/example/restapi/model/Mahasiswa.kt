db26b5a0bc8762f46646637fe114fb801860d4f6
FirdausyTama <atama.firdausy@gmail.com>
2024-12-28 20:18:40 +0700

    mulai

---
+++ app/src/main/java/com/example/restapi/model/Mahasiswa.kt
@@ -0,0 +1,19 @@
+package com.example.restapi.model
+
+import kotlinx.serialization.*
+import kotlinx.serialization.json.*
+import kotlinx.serialization.descriptors.*
+import kotlinx.serialization.encoding.*
+
+@Serializable
+data class Mahasiswa (
+    val nim: String,
+    val nama: String,
+    val alamat: String,
+
+    @SerialName("jenis_kelamin")
+    val jenisKelamin: String,
+
+    val kelas: String,
+    val angkatan: String
+)
