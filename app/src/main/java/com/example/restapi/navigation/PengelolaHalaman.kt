16eb1f511b40d72afbcd9d0b21ac7602dd4879a1
FirdausyTama <atama.firdausy@gmail.com>
2024-12-29 21:50:38 +0700

    menambah halaman route update

--- app/src/main/java/com/example/restapi/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/restapi/navigation/PengelolaHalaman.kt
@@ -6,12 +6,7 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.restapi.ui.view.DestinasiDetail
-import com.example.restapi.ui.view.DestinasiEntry
-import com.example.restapi.ui.view.DestinasiHome
-import com.example.restapi.ui.view.DetailScreen
-import com.example.restapi.ui.view.EntryMhsScreen
-import com.example.restapi.ui.view.HomeScreen
+import com.example.restapi.ui.view.*
 
 @Composable
 fun PengelolaHalaman(navController: NavHostController = rememberNavController()) {
@@ -50,8 +45,23 @@ fun PengelolaHalaman(navController: NavHostController = rememberNavController())
             val nim = backStackEntry.arguments?.getString("nim") ?: ""
             DetailScreen(
                 nim = nim,
+                onNavigateBack = { navController.popBackStack() },
+                onEditClick = {
+                    navController.navigate("${DestinasiUpdate.route}/$nim")
+                }
+            )
+        }
+
+        // Update Screen
+        composable(
+            route = "${DestinasiUpdate.route}/{nim}"
+        ) { backStackEntry ->
+            val nim = backStackEntry.arguments?.getString("nim") ?: ""
+            UpdateScreen(
+                nim = nim,
                 onNavigateBack = { navController.popBackStack() }
             )
         }
+
     }
 }
