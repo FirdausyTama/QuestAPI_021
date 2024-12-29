0456ba4f993c05d614fe28a64b663be925137fa7
FirdausyTama <atama.firdausy@gmail.com>
2024-12-29 15:38:39 +0700

    membuat route halaman detail

--- app/src/main/java/com/example/restapi/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/restapi/navigation/PengelolaHalaman.kt
@@ -2,38 +2,56 @@ package com.example.restapi.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavController
-import androidx.navigation.NavHost
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.restapi.ui.view.DestinasiDetail
 import com.example.restapi.ui.view.DestinasiEntry
 import com.example.restapi.ui.view.DestinasiHome
+import com.example.restapi.ui.view.DetailScreen
 import com.example.restapi.ui.view.EntryMhsScreen
 import com.example.restapi.ui.view.HomeScreen
 
 @Composable
-fun PengelolaHalaman(navController: NavHostController = rememberNavController()){
+fun PengelolaHalaman(navController: NavHostController = rememberNavController()) {
     NavHost(
         navController = navController,
         startDestination = DestinasiHome.route,
         modifier = Modifier,
-    ){
-        composable(DestinasiHome.route){
+    ) {
+        // Home Screen
+        composable(DestinasiHome.route) {
             HomeScreen(
-                navigateToItemEntry = {navController.navigate(DestinasiEntry.route)},
-                onDetailClick = {}
+                navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
+                onDetailClick = { nim ->
+                    navController.navigate("${DestinasiDetail.route}/$nim")
+                }
             )
         }
-        composable(DestinasiEntry.route){
-            EntryMhsScreen(navigateBack = {
-                navController.navigate(DestinasiHome.route){
-                    popUpTo(DestinasiHome.route){
-                        inclusive = true
+
+        // Entry Mahasiswa Screen
+        composable(DestinasiEntry.route) {
+            EntryMhsScreen(
+                navigateBack = {
+                    navController.navigate(DestinasiHome.route) {
+                        popUpTo(DestinasiHome.route) {
+                            inclusive = true
+                        }
                     }
                 }
-            })
+            )
+        }
+
+        // Detail Screen
+        composable(
+            route = "${DestinasiDetail.route}/{nim}",
+        ) { backStackEntry ->
+            val nim = backStackEntry.arguments?.getString("nim") ?: ""
+            DetailScreen(
+                nim = nim,
+                onNavigateBack = { navController.popBackStack() }
+            )
         }
     }
 }
