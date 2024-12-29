91e71eb145fe9c81683b3b02903e597fb476eefb
FirdausyTama <atama.firdausy@gmail.com>
2024-12-29 15:07:02 +0700

    perbaikan pada port

--- app/src/main/java/com/example/restapi/di/MahasiswaContainer.kt
+++ app/src/main/java/com/example/restapi/di/MahasiswaContainer.kt
@@ -1,4 +1,4 @@
-package com.example.restapi.di
+package com.example.restapi.dependenciesinjection
 
 import com.example.restapi.repository.MahasiswaRepository
 import com.example.restapi.repository.NetworkKontakRepository
@@ -7,24 +7,22 @@ import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFact
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
 import retrofit2.Retrofit
-import retrofit2.create
 
 interface AppContainer{
     val kontakRepository: MahasiswaRepository
 }
 
-class MahasiswaContainer:AppContainer{
+class MahasiswaContainer: AppContainer{
 
-    private val baseUrl="http://10.2.2:8000/umyTI/" //localhost diganti ip kalo run di hp
-    private val json= Json { ignoreUnknownKeys = true }
+    private val baseUrl = "http://10.0.2.2:80/umyTI/"
+    private val json = Json{ignoreUnknownKeys = true}
     private val retrofit: Retrofit = Retrofit.Builder()
         .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
         .baseUrl(baseUrl).build()
+
     private val mahasiswaService: MahasiswaService by lazy {
-        retrofit.create(mahasiswaService::class.java)
-    }
+        retrofit.create(MahasiswaService::class.java)}
 
     override val kontakRepository: MahasiswaRepository by lazy {
-        NetworkKontakRepository(mahasiswaService)
-    }
+        NetworkKontakRepository(mahasiswaService)}
 }
